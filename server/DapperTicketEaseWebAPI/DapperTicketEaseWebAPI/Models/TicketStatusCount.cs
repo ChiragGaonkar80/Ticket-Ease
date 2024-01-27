5e8b00113bac5d1d1e917715c4062781ed6babb6
Lakshita Werulkar <156769978+lakshita-w@users.noreply.github.com>
2024-01-27 14:51:31 +0530

    TicketStatusCount.cs Added

---
+++ server/DapperTicketEaseWebAPI/DapperTicketEaseWebAPI/Models/TicketStatusCount.cs
@@ -0,0 +1,8 @@
+﻿namespace DapperTicketEaseWebAPI.Models
+{
+    public class TicketStatusCount
+    {
+        public string status_title { get; set; }
+        public int ticket_count { get; set; }
+    }
+}

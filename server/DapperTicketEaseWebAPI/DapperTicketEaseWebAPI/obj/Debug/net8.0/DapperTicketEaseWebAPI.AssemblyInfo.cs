356e64b943135861bcd51d03ed29d9c591a748d5
sharvanisalgaonkar <157060254+sharvanisalgaonkar@users.noreply.github.com>
2024-02-01 12:03:34 +0530

    API to list all tickets based on priority

--- server/DapperTicketEaseWebAPI/DapperTicketEaseWebAPI/obj/Debug/net8.0/DapperTicketEaseWebAPI.AssemblyInfo.cs
+++ server/DapperTicketEaseWebAPI/DapperTicketEaseWebAPI/obj/Debug/net8.0/DapperTicketEaseWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DapperTicketEaseWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+513dd85f947d7f72b6896259e48849d9b2054ee4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b29c322ec96ba9130aa480f17df23830b20d72b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DapperTicketEaseWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DapperTicketEaseWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
